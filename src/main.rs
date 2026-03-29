80d37211fd434a0e1918b010c540109c8028d0ad
Paul Pleskunov <pp@positroncloud.com>
2026-03-29 16:50:40 -0400

    [DOC1] added licence info header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// ceify 
+//
+// A small command-line utility for converting spectrophotometer output files into a format compatible with CompleteEASE.
+//
+// Copyright (c) 2026 Pavel Pleskunov.
+// 
+// ceify is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or (at
+// your option) any later version.
+//
+// ceify is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
